a4eb0c7ec646affbac7f8d60cf9510c0c399988a
buhala <fix288@gmail.com>
2013-12-30 08:52:05 +0200

    Fixed latest transactions.

--- current.php
+++ current.php
@@ -2,7 +2,7 @@
 include 'functions.php';
 top();
 $minimum=file_get_contents('/home/dogecoin/round');
-exec('echo password | sudo -S -u dogecoin dogecoind listtransactions temp 5000 2>&1',$output);
+exec('echo 282828 | sudo -S -u dogecoin dogecoind listtransactions temp 5000 2>&1',$output);
 $json=json_decode(implode($output,''));
 echo '<div class="page-other">';
 //print_r($json);
@@ -16,14 +16,16 @@ foreach($json as $transaction){
 //print_r($json);
 $a=remainingToRound();
 echo '<h1 style="text-align:center">Time until next round</h1><h2 style="text-align:center;"><span class="attention">'.$a.'</span></h2><br><h1 style="text-align:center;">Send bets to:<br><span class="attention">'.getDynamic('bet_addr').'</span></h1><h1 style="text-align:center">Transactions</h1><pre style="text-align:center">';
+$i=0;
 foreach($transactions as $t){
-	$i++;
+	
 	if($i<=5){
 		echo '[UP]['.$transactions[$i]->time.'] FROM '.$transactions[$i]->txid.' GOT '.$transactions[$i]->amount."\n";
 	}
 	else{
 		break;
 	}
+	$i++;
 }
 if($i==0){
 	echo 'No transactions have been made this round!';
